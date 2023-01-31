c3e8664a3c8ed3ff752635b39d752dd90c35961b
raebrown-ctrl <brownrae6@gmail.com>
2023-01-30 21:20:52 -0800

    4

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 
 /**
  * main - Prints "_putchar" followed by a new line.
@@ -16,6 +16,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-
 	return (0);
 }
