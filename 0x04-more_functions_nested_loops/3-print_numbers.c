e10a0813b84d6d911d242170c21efb309a6b5fe6
raebrown-ctrl <brownrae6@gmail.com>
2023-02-06 20:15:33 -0800

    speak

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - print the numbers from 0 to 9.
+ *
+ * return: the numbers from 0 to 9
+ */
+void print_numbers(void)
+{
+	int x;
+		for (x = 48; x <= 57; x++)
+	{
+		_putchar(x);
+	}
+
+	_putchar('\n');
+}
