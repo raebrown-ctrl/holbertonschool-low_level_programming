b44bd4371c9e90aff7508cf257f6ad34f5eecaf1
raebrown-ctrl <brownrae6@gmail.com>
2023-02-20 20:02:25 -0800

    square

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* raiz - calculate square
+* @n: base number
+* @aux: square
+* Return: square
+**/
+int raiz(int n, int aux)
+{
+	if (aux * aux == n)
+		return (aux);
+	else if (aux * aux > n)
+		return (-1);
+	aux = raiz(n, aux + 1);
+	return (aux);
+}
+/**
+* _sqrt_recursion - the natural square root of a number
+* @n: the number
+* Return: -1 n does not have natural square root
+**/
+int _sqrt_recursion(int n)
+{
+	int x = raiz(n, 1);
+
+	return (x);
+}
+
