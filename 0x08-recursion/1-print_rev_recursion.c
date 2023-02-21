df395e7465b915c0e18bed544ad667d58ee3266b
raebrown-ctrl <brownrae6@gmail.com>
2023-02-20 19:36:18 -0800

    rev

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _print_rev_recursion - prints a string inverse
+* @s: string to print
+* Return: print
+*/
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
+
