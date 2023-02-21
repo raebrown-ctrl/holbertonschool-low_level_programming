823bfbd4d9b6858bd4140d5ff39a06655724e5c2
raebrown-ctrl <brownrae6@gmail.com>
2023-02-20 21:05:11 -0800

    number 2

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 
 /**
-* main - prints all arguments
-* @argc: arguments count
-* @argv: arguments vector
-* Return: 0 success
-**/
+ *main - entry point
+ *@argc: number of arguments called in compilation.
+ *@argv: array that has the elements used to compile
+ *Return: always 0
+ **/
+
 int main(int argc, char *argv[])
 {
 	int i;
 
+	if (!*argv || argc <= 0)
+		return (-1);
 	for (i = 0; i < argc; i++)
-	{
-		printf("%s\n", argv[i]);
-	}
+		printf("%s\n", *(argv + i));
+
 	return (0);
 }
 
